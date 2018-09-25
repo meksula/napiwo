9d56d199a963960f4c37cdf36f00a52f1f3dc7ff
meksula <meksula.karol.93@gmail.com>
2018-09-25 20:23:35 +0200

    Init commit: poject gen. server + client

---
+++ napiwo-server-1.0/src/main/java/pl/napiwo/NapiwoServerApplication.java
@@ -0,0 +1,12 @@
+package pl.napiwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NapiwoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NapiwoServerApplication.class, args);
+	}
+}
