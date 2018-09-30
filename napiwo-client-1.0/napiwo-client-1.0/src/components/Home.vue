aa04a197f93cc3f8e43ce1217e699d6213699c4c
meksula <meksula.karol.93@gmail.com>
2018-09-30 19:57:24 +0200

    Standalone app init

---
+++ napiwo-client-1.0/napiwo-client-1.0/src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+    <h1>vue app</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ napiwo-standalone-1.0/src/main/java/pl/napiwo/standalone/NapiwoStandaloneApplication.java
@@ -0,0 +1,12 @@
+package pl.napiwo.standalone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NapiwoStandaloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NapiwoStandaloneApplication.class, args);
+    }
+}
